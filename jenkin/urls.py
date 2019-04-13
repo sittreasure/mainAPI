2c8b086f0246824058e3ea57dc3ce4e714768d49
Puripat Arayasirikul <boat@Puripats-MacBook-Pro.local>
2019-04-13 16:45:35 +0700

    [UPDATE] add path /build using class jenkins build view

--- jenkin/urls.py
+++ jenkin/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import JenkinsView
+from .views import JenkinsView, JenkinsBuildView
 
 app_name = 'jenkin'
 
 urlpatterns = [
-  path(r'', JenkinsView.as_view())
+  path(r'', JenkinsView.as_view()),
+  path(r'build/', JenkinsBuildView.as_view())
 ]
