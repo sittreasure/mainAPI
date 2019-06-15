5a0e306c0edf5f9ad9acc1ce30f5ce58550f76f2
Puripat Arayasirikul <boat@Puripats-MacBook-Pro.local>
2019-06-15 22:29:25 +0700

    [UPDATE] add route fb-login

--- user/urls.py
+++ user/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from .views import UserViewSet
+from .views import UserViewSet, FacebookAuthView
 
 router = routers.DefaultRouter()
 
 router.register(r'', UserViewSet)
 
 urlpatterns = [
+  path(r'fb-login/', FacebookAuthView.as_view()),
   path(r'', include(router.urls))
 ]
