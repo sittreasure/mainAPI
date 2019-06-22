40bcfa77844afd300c26306fe38ec689fafec91f
Puripat Arayasirikul <boat@Puripats-MacBook-Pro.local>
2019-06-22 21:49:13 +0700

    [UPDATE] add route me

--- user/urls.py
+++ user/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from .views import UserViewSet, FacebookAuthView
+from .views import UserViewSet, FacebookAuthView, MeView
 
 router = routers.DefaultRouter()
 
 router.register(r'', UserViewSet)
 
 urlpatterns = [
+  path(r'me/', MeView.as_view()),
   path(r'fb-login/', FacebookAuthView.as_view()),
   path(r'', include(router.urls))
 ]
