cec6ad5abc250fd4d75a57b928b73d3cf6631456
Boat <boat46450@hotmail.com>
2019-09-09 23:44:43 +0700

    :sparkles: create view for lesson group, lesson, learning

--- lessons/views.py
+++ lessons/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import LessonGroup, Lesson, Learning
+from .serializers import LessonGroupSerializer, LessonSerializer, LearningSerializer
+from base.jwt import getUserId
+
+class LessonGroupViewSet(viewsets.ModelViewSet):
+  queryset = LessonGroup.objects.all()
+  serializer_class = LessonGroupSerializer
+
+class LessonViewSet(viewsets.ModelViewSet):
+  queryset = Lesson.objects.all()
+  serializer_class = LessonSerializer
+
+class LearningViewSet(viewsets.ModelViewSet):
+  queryset = Learning.objects.all()
+  serializer_class = LearningSerializer
+  def get_queryset(self):
+    queryset = self.queryset
+    userId = getUserId(self.request)
+    return queryset.filter(user=userId)
