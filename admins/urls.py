83d293ec7ab8b19ce4546c7c91b083705dfcccf3
Boat <boat46450@hotmail.com>
2019-10-20 02:31:18 +0700

    :sparkles: add path chart stat

--- admins/urls.py
+++ admins/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import CardStatView
+from .views import CardStatView, ChartStatView
 
 urlpatterns = [
   path(r'cardStat/', CardStatView.as_view()),
+  path(r'chartStat/', ChartStatView.as_view()),
 ]
